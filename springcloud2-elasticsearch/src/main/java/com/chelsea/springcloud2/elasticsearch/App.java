f6b1bf9ee70e1519326c7690d91fe430abe8c3fd
baojun_a <baojun_a@aspirecn.com>
2019-12-23 14:15:03 +0800

    新增es模块

---
+++ springcloud2-elasticsearch/src/main/java/com/chelsea/springcloud2/elasticsearch/App.java
@@ -0,0 +1,13 @@
+package com.chelsea.springcloud2.elasticsearch;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
