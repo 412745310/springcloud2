6cbe66116b88733546516ee7bfb2edd5f54eccd0
baojun_a <baojun_a@aspirecn.com>
2020-02-26 16:06:40 +0800

    新增admin client模块

---
+++ springcloud2-admin-client/src/main/java/com/chelsea/springcloud2/admin/client/controller/SampleController.java
@@ -0,0 +1,23 @@
+package com.chelsea.springcloud2.admin.client.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RefreshScope
+@RestController
+public class SampleController {
+
+    @Value("${user.name}")
+    String userName;
+
+    @Value("${user.age}")
+    int age;
+    
+    @RequestMapping(value = "/echo")
+    public String echo() {
+            return userName + "," + age;
+    }
+
+}
